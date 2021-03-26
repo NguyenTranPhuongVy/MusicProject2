a501ed89c61544a84f8afdd559e9d437b2dac962
Tấn Thanh <tanthanh141214@gmail.com>
2021-03-26 22:11:03 +0700

    push

---
+++ Music.Model/DAO/CommentDAO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Music.Model.EF;
+
+namespace Music.Model.DAO
+{
+    public class CommentDAO
+    {
+        MusicProjectDataEntities db = new MusicProjectDataEntities();
+        public bool Add(Comment comment)
+        {
+            try
+            {
+                comment.comment_active = true;
+                comment.comment_datecreate = DateTime.Now;
+                db.Comments.Add(comment);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+
+        public bool Edit(Comment comment)
+        {
+            try
+            {
+                comment.comment_dateupdate = DateTime.Now;
+                db.Entry(comment).State = System.Data.Entity.EntityState.Modified ;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+
+        public bool Del(int? id)
+        {
+            try
+            {
+                var removeItem = db.Comments.Find(id);
+                foreach(var item in removeItem.Replies)
+                {
+                    ReplyDAO replyDAO = new ReplyDAO();
+                    replyDAO.Del(item.rep_id);
+                }
+                db.Comments.Remove(removeItem);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+    }
+}
---
+++ Music.Model/DAO/ReplyDAO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Music.Model.EF;
+
+namespace Music.Model.DAO
+{
+    public class ReplyDAO
+    {
+        MusicProjectDataEntities db = new MusicProjectDataEntities();
+        public bool Add(Reply reply)
+        {
+            try
+            {
+                reply.rep_active = true;
+                reply.rep_datecreate = DateTime.Now;
+                db.Replies.Add(reply);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+
+        public bool Edit(Reply reply)
+        {
+            try
+            {
+                reply.rep_dateupdate = DateTime.Now;
+                db.Entry(reply).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+
+        public bool Del(int? id)
+        {
+            try
+            {
+                var removeItem = db.Replies.Find(id);
+                db.Replies.Remove(removeItem);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception) { return false; }
+        }
+    }
+}
