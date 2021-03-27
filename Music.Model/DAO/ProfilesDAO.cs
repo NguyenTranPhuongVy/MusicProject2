7fa47ba13c29871003c993b9eebaa36760ba52dc
tanhuynh1999 <54873015+tanhuynh1999@users.noreply.github.com>
2021-03-27 10:40:51 +0700

    push

--- Music.Model/DAO/ProfilesDAO.cs
+++ Music.Model/DAO/ProfilesDAO.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.Entity;
+//using System.Data.Entity;
 using Music.Model.EF;
 
 namespace Music.Model.DAO
