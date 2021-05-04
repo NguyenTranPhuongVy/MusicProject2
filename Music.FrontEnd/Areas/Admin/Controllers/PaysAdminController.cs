aebff53e53cdfaf5fa88d2b187683b60726aa34f
Nguyễn Trần Phương Vy <nguyentranphuongvy99@gmail.com>
2021-05-04 12:05:58 +0700

    Admin

---
+++ Music.FrontEnd/Areas/Admin/Controllers/PaysAdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Music.Model.EF;
+using Music.Model.DAO;
+using Music.FrontEnd.Function;
+using Music.Common;
+
+namespace Music.FrontEnd.Areas.Admin.Controllers
+{
+    public class PaysAdminController : Controller
+    {
+        MusicProjectDataEntities db = new MusicProjectDataEntities();
+        FunctionController function = new FunctionController();
+        FilesController imagesfunction = new FilesController();
+        // GET: Admin/PaysAdmin
+        public ActionResult Index()
+        {
+            return View(db.Pays.OrderByDescending(n => n.pay_datecreate).ToList());
+        }
+    }
+}
