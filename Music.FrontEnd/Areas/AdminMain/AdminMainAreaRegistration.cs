381d1cece5b1bfb9f547a84c1abd5655f6ddf6c4
tanhuynh1999 <54873015+tanhuynh1999@users.noreply.github.com>
2021-05-05 11:13:42 +0700

    push

---
+++ Music.FrontEnd/Areas/AdminMain/AdminMainAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Music.FrontEnd.Areas.AdminMain
+{
+    public class AdminMainAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "AdminMain";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "AdminMain_default",
+                "AdminMain/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Music.FrontEnd/Areas/AdminMain/Controllers/HomeAController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Music.FrontEnd.Areas.AdminMain.Controllers
+{
+    public class HomeAController : Controller
+    {
+        // GET: AdminMain/HomeA
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
