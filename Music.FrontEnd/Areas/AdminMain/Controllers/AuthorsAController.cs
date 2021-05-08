cf7b2643fa118d02c0c27b4dfb54460f29ad3c04
Tấn Thanh <tanthanh141214@gmail.com>
2021-05-08 21:53:15 +0700

    push

--- Music.FrontEnd/Areas/AdminMain/Controllers/AuthorsAController.cs
+++ Music.FrontEnd/Areas/AdminMain/Controllers/AuthorsAController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Music.FrontEnd.Function;
 using Music.Model.EF;
 
 namespace Music.FrontEnd.Areas.AdminMain.Controllers
@@ -13,11 +14,12 @@ namespace Music.FrontEnd.Areas.AdminMain.Controllers
     public class AuthorsAController : Controller
     {
         private MusicProjectDataEntities db = new MusicProjectDataEntities();
+        private FilesController filesController = new FilesController();
 
         // GET: AdminMain/AuthorsA
         public ActionResult Index()
         {
-            return View(db.Authors.OrderByDescending(n=>n.author_name).ToList());
+            return View(db.Authors.Where(x => x.author_bin == false).OrderByDescending(n=>n.author_name).ToList());
         }
 
         // GET: AdminMain/AuthorsA/Details/5
@@ -46,10 +48,13 @@ namespace Music.FrontEnd.Areas.AdminMain.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "author_id,author_name,author_active,author_bin,author_note,author_datecreate,author_dateupdate,author_img")] Author author)
+        public ActionResult Create([Bind(Include = "author_id,author_name,author_active,author_bin,author_note,author_datecreate,author_dateupdate,author_img")] Author author, HttpPostedFileBase img)
         {
             if (ModelState.IsValid)
             {
+                author.author_bin = false;
+                author.author_datecreate = DateTime.Now;
+                author.author_img = filesController.AddImages(img, "Author", Guid.NewGuid().ToString());
                 db.Authors.Add(author);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -78,10 +83,14 @@ namespace Music.FrontEnd.Areas.AdminMain.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "author_id,author_name,author_active,author_bin,author_note,author_datecreate,author_dateupdate,author_img")] Author author)
+        public ActionResult Edit([Bind(Include = "author_id,author_name,author_active,author_bin,author_note,author_datecreate,author_dateupdate,author_img")] Author author, HttpPostedFileBase img)
         {
             if (ModelState.IsValid)
             {
+                if(img != null)
+                {
+                    author.author_img = filesController.AddImages(img, "Author", Guid.NewGuid().ToString());
+                }
                 db.Entry(author).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -101,7 +110,25 @@ namespace Music.FrontEnd.Areas.AdminMain.Controllers
             {
                 return HttpNotFound();
             }
-            return View(author);
+            db.Authors.Find(id).author_bin = true;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ChangeActive(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Author author = db.Authors.Find(id);
+            if (author == null)
+            {
+                return HttpNotFound();
+            }
+            db.Authors.Find(id).author_active = !db.Authors.Find(id).author_active;
+            db.SaveChanges();
+            return Json(true, JsonRequestBehavior.AllowGet);
         }
 
         // POST: AdminMain/AuthorsA/Delete/5
--- Music.FrontEnd/Areas/AdminMain/Controllers/UsersAController.cs
+++ Music.FrontEnd/Areas/AdminMain/Controllers/UsersAController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Music.FrontEnd.Function;
 using Music.Model.EF;
 
 namespace Music.FrontEnd.Areas.AdminMain.Controllers
@@ -13,21 +14,22 @@ namespace Music.FrontEnd.Areas.AdminMain.Controllers
     public class UsersAController : Controller
     {
         private MusicProjectDataEntities db = new MusicProjectDataEntities();
+        private FilesController filesController = new FilesController();
 
         // GET: AdminMain/UsersA
         public ActionResult IndexVip()
         {
-            var users = db.Users.Where(n => n.role_id == 1 && n.user_vip == true).OrderByDescending(n => n.user_datecreate).Include(u => u.Role);
+            var users = db.Users.Where(n => n.role_id == 1 && n.user_vip == true && n.user_bin == false).OrderByDescending(n => n.user_datecreate).Include(u => u.Role);
             return View(users.ToList());
         }
         public ActionResult IndexAdmin()
         {
-            var users = db.Users.Where(n => n.role_id == 2).OrderByDescending(n => n.user_datecreate).Include(u => u.Role);
+            var users = db.Users.Where(n => n.role_id == 2 && n.user_bin == false).OrderByDescending(n => n.user_datecreate).Include(u => u.Role);
             return View(users.ToList());
         }
         public ActionResult Index()
         {
-            var users = db.Users.Where(n=>n.role_id == 1 && n.user_vip == false).OrderByDescending(n=>n.user_datecreate).Include(u => u.Role);
+            var users = db.Users.Where(n=>n.role_id == 1 && n.user_vip == false && n.user_bin == false).OrderByDescending(n=>n.user_datecreate).Include(u => u.Role);
             return View(users.ToList());
         }
 
@@ -49,24 +51,56 @@ namespace Music.FrontEnd.Areas.AdminMain.Controllers
         // GET: AdminMain/UsersA/Create
         public ActionResult Create()
         {
-            ViewBag.role_id = new SelectList(db.Roles, "role_id", "role_name");
+            ViewBag.roleCreate = 1;
             return View();
         }
+
+        public ActionResult CreateUserVip()
+        {
+            ViewBag.roleCreate = 2;
+            return View("Create");
+        }
+
+        public ActionResult CreateAdmin()
+        {
+            ViewBag.roleCreate = 3;
+            return View("Create");
+        }
+
 
         // POST: AdminMain/UsersA/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "user_id,user_name,user_img,user_email,user_pass,user_token,user_datecreate,user_datelogin,user_active,user_option,user_bin,user_code,user_point,user_deadline,user_birth,user_phone,user_note,user_address,user_vip,user_datevip,role_id")] User user)
+        public ActionResult Create([Bind(Include = "user_id,user_name,user_img,user_email,user_pass,user_token,user_datecreate,user_datelogin,user_active,user_option,user_bin,user_code,user_point,user_deadline,user_birth,user_phone,user_note,user_address,user_vip,user_datevip,role_id")] User user, HttpPostedFileBase file_img, string url)
         {
-            if (ModelState.IsValid)
+            Random r = new Random();
+            if(db.Users.FirstOrDefault(x => x.user_email == user.user_email) != null)
             {
+                ViewBag.noti_failed = "Email đã tồn tại!";
+            }
+            else
+            {
+                if(file_img != null)
+                {
+                    user.user_img = filesController.AddImages(file_img, "Users", Guid.NewGuid().ToString());
+                }
+                user.user_datecreate = DateTime.Now;
+                user.user_datelogin = DateTime.Now;
+                user.user_active = true;
+                user.user_bin = false;
+                user.user_option = true;
+                user.user_token = Guid.NewGuid().ToString();
+                do
+                {
+                    user.user_code = "#music-" + r;
+                } while (db.Users.Where(t => t.user_code == user.user_code) != null);
                 db.Users.Add(user);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction(url);
             }
-
+            
             ViewBag.role_id = new SelectList(db.Roles, "role_id", "role_name", user.role_id);
             return View(user);
         }
@@ -83,8 +117,41 @@ namespace Music.FrontEnd.Areas.AdminMain.Controllers
             {
                 return HttpNotFound();
             }
+            ViewBag.roleCreate = 1;
             ViewBag.role_id = new SelectList(db.Roles, "role_id", "role_name", user.role_id);
             return View(user);
+        }
+
+        public ActionResult EditUserVip(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.roleCreate = 2;
+            ViewBag.role_id = new SelectList(db.Roles, "role_id", "role_name", user.role_id);
+            return View("Edit", user);
+        }
+
+        public ActionResult EditAdmin(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.roleCreate = 3;
+            ViewBag.role_id = new SelectList(db.Roles, "role_id", "role_name", user.role_id);
+            return View("Edit", user);
         }
 
         // POST: AdminMain/UsersA/Edit/5
@@ -92,10 +159,18 @@ namespace Music.FrontEnd.Areas.AdminMain.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "user_id,user_name,user_img,user_email,user_pass,user_token,user_datecreate,user_datelogin,user_active,user_option,user_bin,user_code,user_point,user_deadline,user_birth,user_phone,user_note,user_address,user_vip,user_datevip,role_id")] User user)
+        public ActionResult Edit([Bind(Include = "user_id,user_name,user_img,user_email,user_pass,user_token,user_datecreate,user_datelogin,user_active,user_option,user_bin,user_code,user_point,user_deadline,user_birth,user_phone,user_note,user_address,user_vip,user_datevip,role_id")] User user, HttpPostedFileBase IMG)
         {
             if (ModelState.IsValid)
             {
+                //Chèn ảnh
+                if (IMG != null)
+                {
+                    var code = Guid.NewGuid().ToString();
+                    var img = new FilesController();
+                    img.AddImages(IMG, Common.Link.IMG_USER, code);
+                    user.user_img = code + IMG.FileName;
+                }
                 db.Entry(user).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -116,7 +191,73 @@ namespace Music.FrontEnd.Areas.AdminMain.Controllers
             {
                 return HttpNotFound();
             }
-            return View(user);
+            db.Users.Find(id).user_bin = true;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteVip(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            db.Users.Find(id).user_bin = true;
+            db.SaveChanges();
+            return RedirectToAction("IndexVip");
+        }
+
+        public ActionResult DeleteAdmin(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            db.Users.Find(id).user_bin = true;
+            db.SaveChanges();
+            return RedirectToAction("IndexAdmin");
+        }
+
+        public ActionResult ChangeActive(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            db.Users.Find(id).user_active = !db.Users.Find(id).user_active;
+            db.SaveChanges();
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ChangeOption(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            db.Users.Find(id).user_option = !db.Users.Find(id).user_option;
+            db.SaveChanges();
+            return Json(true, JsonRequestBehavior.AllowGet);
         }
 
         // POST: AdminMain/UsersA/Delete/5
